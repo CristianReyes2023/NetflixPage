02d5ce6b64da70c6ed26a1b1835bb178cbae8963
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-08-24 17:06:44 -0500

    bloquesPrecios

--- main.js
+++ main.js
@@ -20,11 +20,11 @@
 
 const path = "config";
 
-let construirNavbar = async()=>{
-    let peticion= await fetch(`${path}.json`);
+let construirNavbar = async () => {
+    let peticion = await fetch(`${path}.json`);
     let res = await peticion.json();
     let selecion = document.querySelector("#navBar");
-        selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    selecion.insertAdjacentHTML("beforeend",/*html*/ `
         <img
             class="netFliximg"
             src="assets/brand/Netflix_2015_logo.svg">
@@ -34,22 +34,43 @@ let construirNavbar = async()=>{
 construirNavbar();
 
 
-let construirElEncabezado = async()=>{
-    let peticion= await fetch(`${path}.json`);
+let construirElEncabezado = async () => {
+    let peticion = await fetch(`${path}.json`);
     let res = await peticion.json();
     let selecion = document.querySelector("#myJsonSection");
-        selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    selecion.insertAdjacentHTML("beforeend",/*html*/ `
         <p class="fs-6">${res.section.step}</p>
         <h1 class="display-4 fw-normal text-body-emphasis fs-1 fw-semibold">${res.section.titulo}</h1>
         <p class="parrafoSection">
-            ${res.section.parrafo.map((value)=>/*html*/`
+            ${res.section.parrafo.map((value) =>/*html*/`
             <svg class="bi2 me-2 " width="24" height="24">
             <use xlink:href="#check" />
-          </svg>${value.texto}<br>`).join(" ")}
+            </svg>${value.texto}<br>`).join(" ")}
         </p>
     `)
 }
 construirElEncabezado();
+
+let construirPlanes = async () => {
+    let peticion = await fetch(`${path}.json`);
+    let res = await peticion.json();
+    let selecion = document.querySelector("#myPlans");
+    selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    ${res.bloques.map((value) =>/*html*/`
+    <div class="col">  
+        <div class="card mb-4 rounded-0 shadow-sm">
+            <div class="card-header rounded-0 py-3 bg-danger-subtle">
+                <h4 class="my-0 fw-normal text-light">${value.plan}</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title fs-2">${value.precio}<small
+                    class="text-body-secondary fw-light">/mo</small>
+                </h1>
+            </div>
+        </div>
+    </div>`).join(" ")}`)
+}
+construirPlanes();
 
 
 
@@ -115,7 +136,7 @@ construirElEncabezado();
 
 
 
-//Tenemos temporizadores 
+//Tenemos temporizadores
 //setTimeour()
 //setInterval()
 
